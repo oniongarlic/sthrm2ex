b74344cbdb9285341d1397a31ac527d94a6e1e62
Kaj-Michael Lang <milang@tal.org>
2013-12-07 01:02:06 +0200

    Add interface for device selection

---
+++ src/sthrm2ex/BtDeviceSelected.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sthrm2ex;
+
+/**
+ *
+ * @author milang
+ */
+public interface BtDeviceSelected {
+    void btDeviceSelected(String address);    
+}
